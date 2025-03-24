9f847b196110764b91910e2971838c84e744d70c
homesh2005 <homeshdega7@gmail.com>
2025-03-24 20:41:52 +0530

    Create server.js

---
+++ models/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const predictionRoutes = require('./routes/predictionRoutes');
+app.use('/api/predictions', predictionRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
